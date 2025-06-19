2fea144e5a91998a64dc7ddf5cf32033a6379cc2
{Mazen-032} <{mazenmagdy337@gmail.com}>
2025-06-19 06:26:36 +0300

    Workspace Setup

--- network/models.py
+++ network/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def is_valid_user(self):
+        return self.first_name != self.last_name
--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
+from .models import User
+
 # Create your tests here.
+class UserTestCase(TestCase):
+
+    def setUp(self):
+
+        # Create user
+        User.objects.create(username="mazen", first_name="Mazen", last_name="Magdy")
+
+
+    def test_valid_user(self):
+        """ Check first name not equal last name """
+        user = User.objects.get(username="mazen")
+        self.assertTrue(user.is_valid_user())
